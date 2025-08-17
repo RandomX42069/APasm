f08e15c62967dbcc25ce7cae15bde2ed2c03003b
RandomX_001 <dungnam120314@gmail.com>
2025-08-17 20:35:25 +0700

    Delete APasm_ROOT directory

--- APasm_ROOT/instruction/cli.py
+++
@@ -1,8 +0,0 @@
-def cli():
-    """
-    Clear Maskable Interrupt
-    To make easier to understand,
-    disable all interrupt that are allowed to
-    be disabled
-    """
-    return bytes([0xFA])
--- APasm_ROOT/instruction/halt.py
+++
@@ -1,3 +0,0 @@
-def hlt():
-    """Halt CPU until next interrupt"""
-    return bytes([0xF4])
